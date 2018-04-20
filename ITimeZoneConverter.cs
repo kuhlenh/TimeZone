725dc148649c95ec0b7642204c8d44c8c10c8059
Kasey Uhlenhuth <kaseyu@microsoft.com>
2018-04-20 15:14:44 -0700

    Add time zone implementation

---
+++ ITimeZoneConverter.cs
@@ -0,0 +1,9 @@
+﻿using System.Threading.Tasks;
+
+namespace BusInfo
+{
+    public interface ITimeZoneConverter
+    {
+        Task<string> GetTimeZoneJsonFromLatLonAsync(string lat, string lon);
+    }
+}
---
+++ TimeZoneConverter.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Net.Http;
+using System.Threading.Tasks;
+
+namespace BusInfo
+{
+    public class TimeZoneConverter : ITimeZoneConverter
+    {
+        HttpClient http = new HttpClient();
+        private const string Key = "demo";
+
+        public async Task<string> GetTimeZoneJsonFromLatLonAsync(string lat, string lon)
+        {
+            var unix = new DateTimeOffset(DateTime.UtcNow).ToUnixTimeSeconds();
+            var url = $"https://maps.googleapis.com/maps/api/timezone/json?location={lat},{lon}&timestamp={unix}&sensor=false";
+            var response = await http.GetAsync(url);
+            if (!response.IsSuccessStatusCode)
+            {
+                return "";
+            }
+            var json = await response.Content.ReadAsStringAsync();
+            return json;
+        }
+    }
+}
